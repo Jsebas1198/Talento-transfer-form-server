5b2168efebc05ba0649b4c63180a6848bd8a772c
Juan Sebastian Fernandez Saez <jsebas1198@protonmail.com>
2023-05-24 16:21:56 -0400

    Change app port and enable global pipes and CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //If the atribute is not in DTO, it dont enter the payload
+      forbidNonWhitelisted: true, //It alerts de API if t
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
